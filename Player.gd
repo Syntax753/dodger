17c360c395eb45c7ede72e960553ea65724d3bda
Peter Turner <syntax.valid@gmail.com>
2019-04-28 21:37:51 +0100

    Touch screen instead of keyboard

--- Player.gd
+++ Player.gd
@@ -4,21 +4,37 @@ signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
+var target = Vector2()
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
+
+func start(pos):
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 	
 func _process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	if position.distance_to((target) > 10:
+		velocity = (target-poition).normalized*speed
+	else:
+		velocity = Vector2()
+	
+#	var velocity = Vector2()  # The player's movement vector.
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -27,8 +43,9 @@ func _process(delta):
     	$AnimatedSprite.stop()
 
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	# No need to clamp as can't press outside the screen
+#	position.x = clamp(position.x, 0, screen_size.x)
+#	position.y = clamp(position.y, 0, screen_size.y)
 
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
@@ -43,10 +60,5 @@ func _on_Player_body_entered(body):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.call_deferred("set_disabled",true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
 
 
